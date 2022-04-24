85d68d7fdfd74e6f8803cbd457ecae5147cd94b5
AjabiOladipupo <66166525+AjabiOladipupo@users.noreply.github.com>
2022-04-24 15:42:04 +0100

    printf.c edited

--- _printf.c
+++ _printf.c
@@ -3,13 +3,26 @@
 
 int _printf(const char * const format, ...)
 {
+	va_list args;
 	int i = 0;
+
+	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
-		_putchat(format[i]);
+		if (format[i] != '%')
+		    _putchat(format[i]);
+		else
+		{
+			/* format[i] == '%' */
+			if (format[i+1] == 'c')
+			_putchar(va_arg(args, int));
+		}
+
 		i++;
 	}
+
+	va_end(args);
 
 	return (0);
 
