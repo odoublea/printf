c368ee0a03709f9a3f20aad91e80890c77848adb
unblac <55189801+unblac@users.noreply.github.com>
2022-04-24 23:01:42 +0100

    strings formatter

--- _printf.c
+++ _printf.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stddef.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0;
+	int i = 0, j= 0;
+	char *str = NULL;
 
 	va_start(args, format);
 
@@ -20,6 +22,17 @@ int _printf(const char *format, ...)
 			{
 				_putchar(va_arg(args, int));
 				i++;
+			}
+			else if (format[i+1] == 's')
+			{
+				i++;
+				str = va_arg(args, char *);
+				j = 0;
+				while (str[j] != '\0')
+				{
+					_putchar(str[j])
+					j++;
+				}
 			}
 		}
 
