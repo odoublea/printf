50a8c16117aad0f5324e15df25ad82146a8e4939
unblac <55189801+unblac@users.noreply.github.com>
2022-04-25 16:04:15 +0100

    Betty style

--- _printf.c
+++ _printf.c
@@ -2,10 +2,17 @@
 #include <stddef.h>
 #include <stdarg.h>
 
+/**
+ * check_for_specifiers - checks if there is a valid format specifer
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, j= 0;
+	int i = 0, j = 0;
 	char *str = NULL;
 
 	va_start(args, format);
@@ -14,16 +21,16 @@ int _printf(const char *format, ...)
 	{
 
 		if (format[i] != '%')
-		    _putchar(format[i]);
+			_putchar(format[i]);
 		else
 		{
 			/* format[i] == '%' */
-			if (format[i+1] == 'c')
+			if (format[i + 1] == 'c')
 			{
 				_putchar(va_arg(args, int));
 				i++;
 			}
-			else if (format[i+1] == 's')
+			else if (format[i + 1] == 's')
 			{
 				i++;
 				str = va_arg(args, char *);
@@ -42,5 +49,4 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (0);
-
 }
