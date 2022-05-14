489b3534352a233ec7d4a034ae347464e6fb27f6
unblac <55189801+unblac@users.noreply.github.com>
2022-05-14 07:51:19 +0100

    fixing the errors

---
+++ _print_char.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int _print_char(char c)
+{
+    return (_putchar(c));
+}
--- main.h
+++ main.h
@@ -3,5 +3,8 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int _print_char(char c);
+int _print_str(char *s);
+int _print_int(int n);
 
 #endif /* _MAIN_H_ */
